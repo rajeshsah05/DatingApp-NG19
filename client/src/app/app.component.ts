e2b463e0d89013af566bf49d6c98f0533047edf7
Rajesh Sah <rajeshmicrosoft05@gmail.com>
2025-07-26 15:23:28 +0530

    End Of Section 6

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,42 +1,13 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NavComponent } from "../layout/nav/nav.component";
-import { AccountService } from '../core/services/account.service';
-import { lastValueFrom } from 'rxjs';
-import { HomeComponent } from "../features/home/home.component";
-import { User } from '../types/user';
+import { Router, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [NavComponent, HomeComponent],
+  imports: [NavComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-  title = 'Dating App';
-  protected members = signal<User[]>([]);
-
- async ngOnInit() {
-    this.members.set(await this.getMembers());
-    this.setCurrentUser();
-  }
-
-  async getMembers(){
-    try {
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  }
-
-  setCurrentUser(){
-   const userString = localStorage.getItem('user');
-   if(!userString) return;
-   const user = JSON.parse(userString);
-   this.accountService.currentUser.set(user);
-  }
-
+export class AppComponent {
+  protected router = inject(Router);
 }
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,13 +1,33 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { InitService } from '../core/services/init.service';
+import { lastValueFrom } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes),
-    provideHttpClient()
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withViewTransitions()),
+    provideHttpClient(),
+    provideAppInitializer(async () => {
+      const intiService = inject(InitService);
+
+      return new Promise<void>((resolve) => {
+        setTimeout(async () => {
+          try {
+            return lastValueFrom(intiService.init())
+          } finally {
+            const splash = document.getElementById('initial-splash');
+            if (splash) {
+              splash.remove();
+            }
+            resolve();
+          }
+        }, 500);
+      })
+
+    })
   ]
 };
